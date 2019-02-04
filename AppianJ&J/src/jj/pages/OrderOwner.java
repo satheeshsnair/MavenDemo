f326f82165fb30362ca19b9cc0fc8d678505c3fb
satheeshnair <snairsatheesh@gmail.com>
2019-02-04 19:26:06 +0530

    Added new page OrderOwner

---
+++ AppianJ&J/src/jj/pages/OrderOwner.java
@@ -0,0 +1,16 @@
+package jj.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class OrderOwner {
+	
+	public WebDriver Driver;
+	
+	public By submit = By.xpath("//button[@class='Button---btn Button---default_direction Button---primary appian-context-last-in-list']");
+	public By position_ID = By.xpath("(//a[@class='elements---global_a'])[1]");
+	public By accept = By.xpath("//button[@class='Button---btn Button---default_direction Button---primary']");
+	public By reject = By.xpath("//button[@class='Button---btn Button---default_direction Button---destructive appian-context-first-in-list']");
+	public By close = By.xpath("//button[@class='Button---btn Button---default_direction appian-context-last-in-list']");
+	public By comment = By.xpath("//textarea[@class='ParagraphWidget---textarea ParagraphWidget---align_start']");
+}
